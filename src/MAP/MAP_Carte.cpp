72c3574ba97297abe4e3b1d059804d22ac64b7e1
Gabriel <gabriel.harang@ensea.fr>
2015-12-01 10:26:25 +0100

    bwahaha

--- src/MAP/MAP_Carte.cpp
+++ src/MAP/MAP_Carte.cpp
@@ -129,22 +129,14 @@ int MAP_Carte::getLevelValue(int x,int y) const {
 	if(y<0){
 		throw std::invalid_argument("y<0");
 	}
-<<<<<<< HEAD
-	if(y>this->height){
-=======
 	if(y>=height){
 		std::cerr << y << " et " << height << std::endl;
->>>>>>> 7da0617d55ddac569f38cb594e6db72427a9bfa5
 		throw std::invalid_argument("y>taille");
 	}
 	if(x<0){
 		throw std::invalid_argument("x<0");
 	}
-<<<<<<< HEAD
-	if(x>this->width){
-=======
 	if(x>=width){
->>>>>>> 7da0617d55ddac569f38cb594e6db72427a9bfa5
 		throw std::invalid_argument("x>taille");
 	}
 	
